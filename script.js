4ebc4713d992a5ec06394634a9158a154b5c3106
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2021-12-01 07:45:22 -0800

    practice logic to map out conditional logic

---
+++ script.js
@@ -0,0 +1,95 @@
+
+// External files make your code organized and easier to maintain
+console.log("This is the log for the 🔥EXTERNAL🔥JavaScript");
+  
+  
+
+  let studentName = "Kimberly";
+  let studentAge = 33;
+
+
+  console.log("Her name is " + studentName); 
+
+  let personName = 'Juan';
+  let pets = 1;
+  let funFact = 'loves to run';
+
+  console.log('My name is ' + studentName + '.');
+  console.log('I have ' + pets + ' pet(s).');
+  console.log('Fun fact: ' + funFact);
+
+  //When re-assigning variables, use variables name
+personName = 'Mateo';
+pets = 5;
+funFact = 'I was a yo-yo champ in third grade.';
+
+console.log('My name is ' + personName + '.');
+console.log('I have ' + pets + ' pet(s).');
+console.log('Fun fact: ' + funFact); 
+
+let isMyBooleanTrue = true; 
+
+console.log(typeof isMyBooleanTrue);
+
+let a = 100;
+let b = 10;
+let c = "10";
+
+console.log(a + b);
+console.log(a - b);
+console.log(a/b);
+console.log(a*b);
+
+console.log(a%b);
+
+console.log(b == c);
+console.log(b !== c);
+
+console.log(a > b);
+console.log(a < b);
+
+console.log(a <= b);
+console.log(a >= b);
+
+let expression1 = (b == c);
+let expression2 = (a > b);
+
+console.log(expression1 && expression2);
+
+console.log(expression1 || expression2); 
+
+console.log(expression2);
+
+console.log(!expression2);
+
+
+let hungerLevel = 50;
+let isLunchTime = true;
+let lunchBill = 11;
+
+if (hungerLevel >= 50) {
+  console.log("Hungry!");
+}
+
+if (!hungerLevel < 50) {
+  console.log("Hungry, Hungry!");
+}
+
+if(hungerLevel <= 50) {
+  console.log("Hungry again!");
+}
+
+if (!isLunchTime) {
+  console.log("lunch time");
+} else {
+  console.log("Not Lunchtime");
+}
+
+if (hungerLevel){
+  console.log('its lunch time!')
+} else {
+  console.log('its not lunch time')
+}; 
+
+
+
