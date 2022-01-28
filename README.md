09c721ba4740f412b8d5cc933fd4015d170090b2
kimberly Rodriguez <kimberlyrodriguez@kimberlys-iMac.attlocal.net>
2022-01-27 17:08:40 -0800

    update readme with minor add on

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
 # Columbia_Management
+
+## Table of Contents
+
+* [Description](#description)
+* [Instalation](#instalation)
+* [Tools](#tools)
+* [Contact](#contact)
+* [License](#license)
+* [Acknowledgements](#acknowledgements)
+
+## Description
+
 To manage rental listings for private owners
+
+
+## Instalation
+
+* Clone the repo (please reference the links below).
+```md 
+
+
+```
+
+
+[Site link]()
+
+## Tools
+
+* This project was built using HTML, CSS and React principles.
+
+
+## Contact
+
+If you have any questions about my work OR wish to collaborate in the future please contact me via email: krodriguez.ucla@gmail.com OR feel free to connect via GitHub: [Kimberly-Rodriguez](https://github.com/Kimberly-Rodriguez).
+
+## License 
+
+Licensed under the [MIT License](LICENSE).
+
+
--- script.js
+++ script.js
@@ -1,224 +1,332 @@
 
-// External files make your code organized and easier to maintain
-console.log("This is the log for the 🔥EXTERNAL🔥JavaScript");
+// // External files make your code organized and easier to maintain
+// console.log("This is the log for the 🔥EXTERNAL🔥JavaScript");
   
   
 
-  let studentName = "Kimberly";
-  let studentAge = 33;
+//   let studentName = "Kimberly";
+//   let studentAge = 33;
 
 
-  console.log("Her name is " + studentName); 
+//   console.log("Her name is " + studentName); 
 
-  let personName = 'Juan';
-  let pets = 1;
-  let funFact = 'loves to run';
+//   let personName = 'Juan';
+//   let pets = 1;
+//   let funFact = 'loves to run';
 
-  console.log('My name is ' + studentName + '.');
-  console.log('I have ' + pets + ' pet(s).');
-  console.log('Fun fact: ' + funFact);
+//   console.log('My name is ' + studentName + '.');
+//   console.log('I have ' + pets + ' pet(s).');
+//   console.log('Fun fact: ' + funFact);
 
-  //When re-assigning variables, use variables name
-personName = 'Mateo';
-pets = 5;
-funFact = 'I was a yo-yo champ in third grade.';
+//   //When re-assigning variables, use variables name
+// personName = 'Mateo';
+// pets = 5;
+// funFact = 'I was a yo-yo champ in third grade.';
 
-console.log('My name is ' + personName + '.');
-console.log('I have ' + pets + ' pet(s).');
-console.log('Fun fact: ' + funFact); 
+// console.log('My name is ' + personName + '.');
+// console.log('I have ' + pets + ' pet(s).');
+// console.log('Fun fact: ' + funFact); 
 
-let isMyBooleanTrue = true; 
+// let isMyBooleanTrue = true; 
 
-console.log(typeof isMyBooleanTrue);
+// console.log(typeof isMyBooleanTrue);
 
-let a = 100;
-let b = 10;
-let c = "10";
+// let a = 100;
+// let b = 10;
+// let c = "10";
 
-console.log(a + b);
-console.log(a - b);
-console.log(a/b);
-console.log(a*b);
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a/b);
+// console.log(a*b);
 
-console.log(a%b);
+// console.log(a%b);
 
-console.log(b == c);
-console.log(b !== c);
+// console.log(b == c);
+// console.log(b !== c);
 
-console.log(a > b);
-console.log(a < b);
+// console.log(a > b);
+// console.log(a < b);
 
-console.log(a <= b);
-console.log(a >= b);
+// console.log(a <= b);
+// console.log(a >= b);
 
-let expression1 = (b == c);
-let expression2 = (a > b);
+// let expression1 = (b == c);
+// let expression2 = (a > b);
 
-console.log(expression1 && expression2);
+// console.log(expression1 && expression2);
 
-console.log(expression1 || expression2); 
+// console.log(expression1 || expression2); 
 
-console.log(expression2);
+// console.log(expression2);
 
-console.log(!expression2);
+// console.log(!expression2);
 
 
-let hungerLevel = 50;
-let isLunchTime = true;
-let lunchBill = 11;
+// let hungerLevel = 50;
+// let isLunchTime = true;
+// let lunchBill = 11;
 
-if (hungerLevel >= 50) {
-  console.log("Hungry!");
-}
+// if (hungerLevel >= 50) {
+//   console.log("Hungry!");
+// }
 
-if (!hungerLevel < 50) {
-  console.log("Hungry, Hungry!");
-}
+// if (!hungerLevel < 50) {
+//   console.log("Hungry, Hungry!");
+// }
 
-if(hungerLevel <= 50) {
-  console.log("Hungry again!");
-}
+// if(hungerLevel <= 50) {
+//   console.log("Hungry again!");
+// }
 
-if (!isLunchTime) {
-  console.log("lunch time");
-} else {
-  console.log("Not Lunchtime");
-}
+// if (!isLunchTime) {
+//   console.log("lunch time");
+// } else {
+//   console.log("Not Lunchtime");
+// }
 
-if (hungerLevel){
-  console.log('its lunch time!')
-} else {
-  console.log('its not lunch time')
-}; 
+// if (hungerLevel){
+//   console.log('its lunch time!')
+// } else {
+//   console.log('its not lunch time')
+// }; 
 
-let students = ["Juan", "Kim", "Sam", "Mattie", "Karla", "Briana"]
+// let students = ["Juan", "Kim", "Sam", "Mattie", "Karla", "Briana"]
 
-// console.log('the total number of students is '+ students.length)
+// // console.log('the total number of students is '+ students.length)
 
-// console.log('Welcome to class '+ students[0]);
-// console.log('Welcome to class '+ students[1]);
-// console.log('Welcome to class '+ students[2]);
-// console.log('Welcome to class '+ students[3]);
-// console.log('Welcome to class '+ students[4]);
-// console.log('Welcome to class '+ students[5]);    
+// // console.log('Welcome to class '+ students[0]);
+// // console.log('Welcome to class '+ students[1]);
+// // console.log('Welcome to class '+ students[2]);
+// // console.log('Welcome to class '+ students[3]);
+// // console.log('Welcome to class '+ students[4]);
+// // console.log('Welcome to class '+ students[5]);    
 
-students[0]= 'Mark'
-console.log(students);
+// students[0]= 'Mark'
+// console.log(students);
 
-if (students[0] === 'Juan' ) {
- console.log (students[0] + 'is still in class')
-} else {
-  console.log("is a new student by the name of ... " + students[0])
-}
+// if (students[0] === 'Juan' ) {
+//  console.log (students[0] + 'is still in class')
+// } else {
+//   console.log("is a new student by the name of ... " + students[0])
+// }
 
-// for (let i = 0; i < array.length; i++) {
-//   const element = array[i];
+// // for (let i = 0; i < array.length; i++) {
+// //   const element = array[i];
   
+// // }
+
+// // for (let i = 0; i < students.length; i++) {
+// //  console.log('you are my student named '+ students[i])
+// // }
+
+// // iterator let i = 0  --> its a variable that starts at 0 + iterator 
+// // condition i < 6 ---> as long as the value of i is less than 6 pls run the array
+// // incrementor i ++  --> add one to the value of i === i = i + 1
+// for (let i = 0; i < 6; i++) {
+//   console.log("this worked, thanks too ..." + students[i])
+// }; 
+
+// let array = ["fun", "happy", "joy", "springles"]
+
+// // for ( let i = 0; i < array.length; i++ ) {
+// //  console.log('I find joy in my love for' + array[i]); 
+// // }
+
+// // for (let i = 0; i < array.length; i++) {
+// //   const element = 'I work to always be ' + array[i];
+// //   console.log(element);
+// // }
+
+// for (let i = 0; i < array.length ; i++) {
+//   console.log(array[i])
 // }
 
-// for (let i = 0; i < students.length; i++) {
-//  console.log('you are my student named '+ students[i])
+// let classmates = ['Kim', 'Juan', 'Korto', 'Briana', 'Karla'];
+
+// // for (let i = 0; i < classmates.length; i++){
+// //   console.log('Great to see you ' + classmates[i] + '!')
+// // } 
+
+// // classmates()
+
+
+//   classmates.slice(2, -1)
+//   console.log(classmates)
+
+//   var count = 0;
+//   while(count<students.length){
+//     console.log('great to see you' + students[count] + '!');
+//     count++;
+//   }
+
+
+//   function declareHello(myName, funStuff){
+//     console.log('Hello, '+ myName + '! ' + funStuff + ', ' + 'I am a function declaration' + '.');
+//     console.log('----------------------------------------');
+//     //Return stops the execution of a function
+//     return;
+//   }
+// //Function must be called to execute
+//   declareHello('Kimberly', 'Enjoy')
+
+//   //function expression
+
+//   let expressHello = function(myName){
+//     console.log('Hello,' + myName + ','+ ' I am function expression');
+//     console.log("------------------------------");
+//     return;
+//   };
+
+//   expressHello(' Kim');
+//   expressHello(' Porto');
+//   expressHello(' Juan');
+
+// function declarationHelloAgain(x,y,z ) {
+//   console.log("Hello, my parameter's value are " + x + ", "+ y + " ,and " + z);
+//   return;
 // }
 
-// iterator let i = 0  --> its a variable that starts at 0 + iterator 
-// condition i < 6 ---> as long as the value of i is less than 6 pls run the array
-// incrementor i ++  --> add one to the value of i === i = i + 1
-for (let i = 0; i < 6; i++) {
-  console.log("this worked, thanks too ..." + students[i])
-}; 
+// declarationHelloAgain(7, "Hello", true);
 
-let array = ["fun", "happy", "joy", "springles"]
 
-// for ( let i = 0; i < array.length; i++ ) {
-//  console.log('I find joy in my love for' + array[i]); 
+// // using function declaration
+// function isEqual(x, y) {
+// if (x === y){
+// console.log('They are equal in value and type');
+// } else if(x == y) {
+// console.log('They are equal in value but not type');
+// } else {
+// console.log('They are not equal');
+// }
+// return;
 // }
 
-// for (let i = 0; i < array.length; i++) {
-//   const element = 'I work to always be ' + array[i];
-//   console.log(element);
+// isEqual(10,10); 
+
+// isEqual('10', 10);
+
+
+// // Using function expression
+// let isEqualTakeTwo = function(x,y){
+//  if (x === y) {
+//    console.log('They are equal in value and type')
+//  } else if (x == y) {
+//   console.log('they are equal in value but not type')
+//  } else {
+//    console.log('they are not equal')
+//  }
+//  return;
 // }
 
-for (let i = 0; i < array.length ; i++) {
-  console.log(array[i])
-}
+// isEqualTakeTwo(10,10);
+// isEqualTakeTwo('10',10);
+// isEqualTakeTwo(9 , true);
 
-let classmates = ['Kim', 'Juan', 'Korto', 'Briana', 'Karla'];
+// //function declaration
 
-// for (let i = 0; i < classmates.length; i++){
-//   console.log('Great to see you ' + classmates[i] + '!')
-// } 
+// function myName(firstName, lastName ){
+// console.log("Her fullname is " + firstName + ' ' + lastName);
+// return;
+// };
 
-// classmates()
+// myName('Kimberly', 'Rodriguez')
+
+// //function expression
+
+// let myTitle = function(office, title) {
+//   console.log("She worked in the office of " + office + " " + 'as the ' + title + ' at the city of Los Angeles!');
+//   return;
+// };
+
+// myTitle('Budget and Innovation', 'Data Programs Lead')
 
 
-  classmates.slice(2, -1)
-  console.log(classmates)
+// let hello = "Hello";
 
-  var count = 0;
-  while(count<students.length){
-    console.log('great to see you' + students[count] + '!');
-    count++;
+// function sayHello() {
+//   console.log(hello);
+//   return;
+// }
+
+// let sayHelloAgain = function () {
+//   console.log(hello);
+//   return;
+// };
+
+// sayHello();
+// sayHelloAgain();
+
+
+// let shadow = "hello";
+// console.log("lets make this be " + shadow);
+
+// //logs 'Hello'
+// function sayWhat(){
+//   console.log(shadow);
+//   return;
+// }
+
+// //logs 'Goodbye'
+// let sayWhatAgain = function () {
+//   let goodbye = "Goodbye";
+//   return goodbye; 
+// };
+
+// let returnValue = sayWhatAgain();
+// console.log("it worked! " + returnValue);
+
+// //sayGoodbye()
+// sayWhat();
+// sayWhatAgain();
+
+// function byeByeBye() {
+//   let adios = "bye bye now!"
+//   return adios;
+// }
+
+// let newSharedVariable = byeByeBye()
+// console.log(newSharedVariable);
+
+// byeByeBye();
+
+
+const makePortfolios = function() {
+  let count = 0;
+  const portfolios = [];
+  while (count < 10) {
+      const portfolio = function() {
+          return { name: 'Portfolio # ' + count };
+      };
+      portfolios.push(portfolio);
   }
 
+  return portfolios;
+};
 
-  function declareHello(myName, funStuff){
-    console.log('Hello, '+ myName + '! ' + funStuff + ', ' + 'I am a function declaration' + '.');
-    console.log('----------------------------------------');
-    //Return stops the execution of a function
-    return;
-  }
-//Function must be called to execute
-  declareHello('Kimberly', 'Enjoy')
-
-  //function expression
-
-  let expressHello = function(myName){
-    console.log('Hello,' + myName + ','+ ' I am function expression');
-    console.log("------------------------------");
-    return;
-  };
-
-  expressHello(' Kim');
-  expressHello(' Porto');
-  expressHello(' Juan');
-
-function declarationHelloAgain(x,y,z ) {
-  console.log("Hello, my parameter's value are " + x + ", "+ y + " ,and " + z);
-  return;
-}
-
-declarationHelloAgain(7, "Hello", true);
+const portfolios = makePortfolios();
+const number0 = portfolios[0];
+console.log(number0.name);
+const number8 = portfolios[8];
+console.log(number8.name);
 
 
-// using function declaration
-function isEqual(x, y) {
-if (x === y){
-console.log('They are equal in value and type');
-} else if(x == y) {
-console.log('They are equal in value but not type');
-} else {
-console.log('They are not equal');
-}
-return;
-}
-
-isEqual(10,10); 
-
-isEqual('10', 10);
 
 
-// Using function expression
-let isEqualTakeTwo = function(x,y){
- if (x === y) {
-   console.log('They are equal in value and type')
- } else if (x == y) {
-  console.log('they are equal in value but not type')
- } else {
-   console.log('they are not equal')
- }
- return;
-}
 
-isEqualTakeTwo(10,10);
-isEqualTakeTwo('10',10);
-isEqualTakeTwo(9 , 10);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
